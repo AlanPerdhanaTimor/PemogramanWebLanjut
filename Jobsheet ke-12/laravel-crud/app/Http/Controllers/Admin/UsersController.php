b1a7d290d09631e48eaffaba7780ebd097ceb0fc
AlanPerdhanaTimor <erevalan@gmail.com>
2020-04-21 09:45:42 +0700

    jobsheet 12

---
+++ Jobsheet ke-12/laravel-crud/app/Http/Controllers/Admin/UsersController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        return view('admin.users.index')->with('users', $users);
+    }
+
+    public function edit(User $user)
+    {
+        //
+    }
+
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    public function destroy(User $user)
+    {
+        //
+    }
+}
